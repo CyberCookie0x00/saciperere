8ac404709822275874f6aecb54c6edbbbe396089
CyberCookie0x00 <cookiecyber@protonmail.com>
2021-02-13 23:09:46 +0000

    Basic files

--- saciperere.js
+++ saciperere.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 require('dotenv').config();
+const fetch = require('node-fetch')
 
 const client = new Discord.Client();
 
@@ -26,5 +27,10 @@ client.on('message', message => {
  
 
 // THIS  MUST  BE  THIS  WAY
+
+setInterval(async () => {
+    await fetch('https://saciperere.repl.co').then(console.log('Pinged'));
+}, 240000);
+
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
