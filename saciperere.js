f86976e3eb4ddd39909b5c9b9247b27bf26e2139
CyberCookie0x00 <cookiecyber@protonmail.com>
2021-02-14 02:47:05 +0000

    Adding ping command

--- saciperere.js
+++ saciperere.js
@@ -8,22 +8,22 @@ const client = new Discord.Client();
 const prefix = "$";
 
 client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLocaleLowerCase();
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(" ");
+	const command = args.shift().toLocaleLowerCase();
   
-  if(command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply("Pong! Esta mensagem teve uma latência de " + timeTaken + "ms.");
-  }
+	if(command === "ping") {
+		const timeTaken = Date.now() - message.createdTimestamp;
+		message.reply(`Pong! Esta mensagem teve uma latência de ${timeTaken}ms.`);
+	}
 });
 
 //Message when bot is ready
 client.on("ready", () => {
-  console.log("I am ready!");
+	console.log("I am ready!");
 });
 
 /*-------Code for bot-------*/
@@ -33,7 +33,7 @@ client.on("ready", () => {
 
 /*--------24/7-------*/
 setInterval(async () => {
-  await fetch("https://saciperere.repl.co").then(console.log("Pinged"));
+	await fetch("https://saciperere.repl.co").then(console.log("Pinged"));
 }, 240000);
 
 /*-------Discord Connection-------*/
